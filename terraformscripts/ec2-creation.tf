3cf8c92dee0ec110a3884045ecf2da9ce5c15a5e
santhoshi-potturu <78248218+santhoshi-potturu@users.noreply.github.com>
2023-06-06 22:48:49 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "InstanceByTerraform2" {
+resource "aws_instance" "InstanceByTerraform2"{
      ami = "ami-008b85aa3ff5c1b02"
 	 instance_type = "t2.micro"
 	 key_name = "santhoshipotturudevops"
