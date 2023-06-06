c23df3402d60aa57d0e2e54783eaa4b9108154e8
santhoshi-potturu <78248218+santhoshi-potturu@users.noreply.github.com>
2023-06-06 22:59:07 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "InstanceByTerraform2"{
      ami = "ami-008b85aa3ff5c1b02"
 	 instance_type = "t2.micro"
 	 key_name = "santhoshipotturudevops"
-	 security_groups = ["launch-wizard-14"]
+	 security_groups = ["launch-wizard-4"]
 	 tags = {
 	 Name = "tomcatservers"
 	 }
